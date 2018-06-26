03fa7f60802bb56c47538ada730182eb0d852e14
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 19:02:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,9 +27,9 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "
-  SELECT authors.name, subgenres.name FROM series
-  INNER JOIN authors ON series.author_id = authors.id
-  INNER JOIN subgenres ON series.subgenre_id = subgenres.id;
+  SELECT authors.name, subgenres.name FROM ((series
+  INNER JOIN authors ON series.author_id = authors.id)
+  INNER JOIN subgenres ON series.subgenre_id = subgenres.id);
   "
 end
 
