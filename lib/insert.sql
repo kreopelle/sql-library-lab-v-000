3005fe301b51ebdda217d7759a7d4896c9de8782
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 18:38:44 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES
-
+('Lord of the Rings')
 ;
 
 INSERT INTO subgenres (name) VALUES
@@ -7,11 +7,12 @@ INSERT INTO subgenres (name) VALUES
 ;
 
 INSERT INTO authors (name) VALUES
-
+('J.R.R. Tolkein')
 ;
 
 INSERT INTO books (title, year, series_id) VALUES
-
+('The Fellowship of the Ring'),
+('Return of the King')
 ;
 
 INSERT INTO character_books (character_id, book_id) VALUES
