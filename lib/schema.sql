9fecb69319ddb49599821c265d8628c1f250af0b
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 18:31:04 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -5,27 +5,27 @@ CREATE TABLE series (
   sub_genre_id INTEGER
 );
 
-CREATE TABLE series (
+CREATE TABLE sub_genre (
   id INTEGER PRIMARY KEY,
-  
+  name TEXT
 );
 
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+
 );
 
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+
 );
 
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+
 );
 
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
-  
+
 );
