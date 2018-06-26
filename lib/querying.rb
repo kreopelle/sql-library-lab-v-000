ed528e98175dfcdafb1f71f1124019246ac2c907
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 19:12:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,8 +45,9 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "
-  SELECT characters.name, SUM(character_books.book_id)
-
+  SELECT characters.name, SUM(character_books.book_id) FROM characters
+  INNER JOIN character_books
+  
 
 
   "
