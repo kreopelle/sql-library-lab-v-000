5e47b590af4221410a14f403a70f6d2f5cbe0897
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 18:35:41 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES 
+
+;
+
+INSERT INTO series () VALUES 
+
+;
+
+INSERT INTO series () VALUES 
+
+;
+
+INSERT INTO series () VALUES 
+
+;
+
+INSERT INTO series () VALUES 
+
+;
