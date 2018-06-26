9709f9bd264c7fc06c94420b5f8690af3c05b8ef
Kayla Reopelle <kreopelle@gmail.com>
2018-06-26 18:30:53 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  sub_genre_id INTEGER
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
+
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  
+);
